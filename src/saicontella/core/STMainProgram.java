29a33b886f7fb5c180dbaa9f39078f1f19b3ead2
mariosk <mariosk@9516236e-fce0-0c4a-9a21-222ed445ce00>
2008-07-13 07:30:57 +0000

    *** empty log message ***
    git-svn-id: https://mariosk78.dyndns.org/svn/REPOS/trunk/P2PServent@1538 9516236e-fce0-0c4a-9a21-222ed445ce00

--- src/saicontella/core/STMainProgram.java
+++ src/saicontella/core/STMainProgram.java
@@ -14,9 +14,9 @@ public class STMainProgram {
 
     public static void main(String[] args) {
         STLibrary sLibrary = STLibrary.getInstance();
-        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "/phex_SaiconNetwork.hosts");
-        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "/phex.log");
-        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "/phex.error.log");
+        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "\\phex_SaiconNetwork.hosts");
+        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "\\phex.log");
+        sLibrary.removePhexFile(sLibrary.getApplicationLocalPath() + "\\phex.error.log");
         STLoginDialog loginDlg = new STLoginDialog();        
     }
 }
